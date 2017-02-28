94967994bcb1b61bb08853171f6c41d491b38307
Adrien morel <adrien.morel@me.com>
2017-02-28 17:20:38 +0100

    add base classes for activity and fragments

---
+++ src/main/java/com/easy/adri/EasyActivity.java
@@ -0,0 +1,53 @@
+package com.easy.adri;
+
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class EasyActivity extends FragmentActivity {
+
+    // setting locale, for testing only
+    public void setLocale(String language, String country) {
+
+        Locale locale = new Locale(language, country);
+        // here we update locale for date formatters
+        Locale.setDefault(locale);
+        // here we update locale for app resources
+        Resources res = getResources();
+        Configuration config = res.getConfiguration();
+        config.locale = locale;
+        res.updateConfiguration(config, res.getDisplayMetrics());
+    }
+
+    public void hideStatusBar() {
+
+        // make the game fullscreen
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        int index = getSupportFragmentManager().getBackStackEntryCount() - 1;
+
+        if (index < 0)
+            return;
+
+        FragmentManager.BackStackEntry backEntry = getSupportFragmentManager().getBackStackEntryAt(index);
+        String tag = backEntry.getName();
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag(tag);
+        if (fragment instanceof EasyFragment)
+            ((EasyFragment) fragment).onTopOfStack();
+    }
+}
---
+++ src/main/java/com/easy/adri/EasyFragment.java
@@ -0,0 +1,26 @@
+package com.easy.adri;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+
+public class EasyFragment extends Fragment {
+
+    public String TAG = "EasyFragment";
+
+    public void pushFragment(EasyFragment fragment, int containerId, String TAG) {
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(containerId, fragment, TAG);
+        fragmentTransaction.addToBackStack(TAG);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.commit();
+
+    }
+
+    public void onTopOfStack() {
+        Log.d("adrien", "Now on top of the stack :)");
+    }
+}
