381ef8557b5d0709fc52fe3cf845f50a057eeb4a
Adrien morel <adrien.morel@me.com>
2017-04-03 23:52:06 +0200

    make testable interface + helper funcs

--- src/main/java/com/easy/adri/EasyFragment.java
+++ src/main/java/com/easy/adri/EasyFragment.java
@@ -2,15 +2,20 @@ package com.easy.adri;
 
 import android.icu.util.TimeUnit;
 import android.os.Handler;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.telecom.Call;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class EasyFragment extends Fragment {
 
     public String TAG = "EasyFragment";
+    private JavaHelpers.Callback testCallback;
 
     public void pushFragment(EasyFragment fragment, int containerId, String TAG) {
 
@@ -29,5 +34,23 @@ public class EasyFragment extends Fragment {
 
     public boolean guardAttached() {
         return getActivity() == null || !isAdded();
+    }
+
+    @Override public void onStart() {
+        super.onStart();
+
+        if (testCallback != null)
+            testCallback.call();
+        testCallback = null;
+    }
+
+    public void testWhenStarted(final JavaHelpers.Callback done) {
+
+        testCallback = new JavaHelpers.Callback() {
+            @Override
+            public void call() {
+                done.call();
+            }
+        };
     }
 }
---
+++ src/main/java/com/easy/adri/Testable.java
@@ -0,0 +1,9 @@
+package com.easy.adri;
+
+/**
+ * Created by adrienmorel on 24/03/2017.
+ */
+
+public interface Testable {
+    public void test(JavaHelpers.CallBackWithArg<Boolean> done);
+}
